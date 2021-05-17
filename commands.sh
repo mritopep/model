b983b6ef39572f56ee3106e41f85936841bb4ffe
tyfl056 <mannatant@gmail.com>
2021-05-17 13:55:42 +0530

    added commands.sh

---
+++ commands.sh
@@ -0,0 +1,14 @@
+mkdir test_data
+mkdir train_data
+
+mkdir train_img
+mkdir train_img/A
+mkdir train_img/B
+
+mkdir test_img
+mkdir test_img/A
+mkdir test_img/B
+
+mkdir img_data
+mkdir img_data/test
+mkdir img_data/train
