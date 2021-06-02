3923783f6d454871e6f3e0859cf85fd47dd4bbd8
Antony J Malakkkaran <antonyjm462@gmail.com>
2021-06-02 10:31:33 +0530

    saving model added

--- arch_util.py
+++ arch_util.py
@@ -3,7 +3,8 @@ import numpy as np
 import scipy.stats as st
 from keras.layers import Layer
 import keras.backend as K
-
+from tensorflow.keras.layers import Dense, Lambda, Dot, Activation, Concatenate
+from tensorflow.keras.layers import Layer,InputLayer
 
 def gaussian_filter_block(input_layer,
                           kernel_size=3,
@@ -120,3 +121,34 @@ def gaussian_filter_block(input_layer,
 #     # we add eps for numerical stability
 #     normalised_mask = mask / (tf.reduce_sum(mask, 1, keep_dims=True) + 1e-8)
 #     return normalised_mask
+
+class Attention(Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __call__(self, inputs):
+        hidden_states = inputs
+        hidden_size = int(hidden_states.shape[2])
+        # Inside dense layer
+        #              hidden_states            dot               W            =>           score_first_part
+        # (batch_size, time_steps, hidden_size) dot (hidden_size, hidden_size) => (batch_size, time_steps, hidden_size)
+        # W is the trainable weight matrix of attention Luong's multiplicative style score
+        score_first_part = Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
+        #            score_first_part           dot        last_hidden_state     => attention_weights
+        # (batch_size, time_steps, hidden_size) dot   (batch_size, hidden_size)  => (batch_size, time_steps)
+        h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
+        score = Dot(axes=[1, 2], name='attention_score')([h_t, score_first_part])
+        attention_weights = Activation('softmax', name='attention_weight')(score)
+        # (batch_size, time_steps, hidden_size) dot (batch_size, time_steps) => (batch_size, hidden_size)
+        context_vector = Dot(axes=[1, 1], name='context_vector')([hidden_states, attention_weights])
+        pre_activation = Concatenate(name='attention_output')([context_vector, h_t])
+        attention_vector = Dense(self.units, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
+        return attention_vector
+
+    def get_config(self):
+        return {'units': self.units}
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
