d6c8d34768b9c015f7b58c3dab23b0c85a6f7efb
Antony J Malakkkaran <antonyjm462@gmail.com>
2021-06-02 11:19:53 +0530

    google client added

--- arch_util.py
+++ arch_util.py
@@ -5,6 +5,8 @@ from keras.layers import Layer
 import keras.backend as K
 from tensorflow.keras.layers import Dense, Lambda, Dot, Activation, Concatenate
 from tensorflow.keras.layers import Layer,InputLayer
+from keras.layers import Dense, Flatten, Reshape, Input, InputLayer
+from keras.models import Sequential, Model
 
 def gaussian_filter_block(input_layer,
                           kernel_size=3,
@@ -129,26 +131,61 @@ class Attention(Layer):
 
     def __call__(self, inputs):
         hidden_states = inputs
-        hidden_size = int(hidden_states.shape[2])
-        # Inside dense layer
-        #              hidden_states            dot               W            =>           score_first_part
-        # (batch_size, time_steps, hidden_size) dot (hidden_size, hidden_size) => (batch_size, time_steps, hidden_size)
-        # W is the trainable weight matrix of attention Luong's multiplicative style score
-        score_first_part = Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
-        #            score_first_part           dot        last_hidden_state     => attention_weights
-        # (batch_size, time_steps, hidden_size) dot   (batch_size, hidden_size)  => (batch_size, time_steps)
-        h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
+        hidden_size = hidden_states.shape
+        flattened = Flatten()(hidden_states)
+        score_first_part = Dense(np.prod(hidden_size), use_bias=False, name='attention_score_vec')(flattened)
+        h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(flattened)
         score = Dot(axes=[1, 2], name='attention_score')([h_t, score_first_part])
         attention_weights = Activation('softmax', name='attention_weight')(score)
-        # (batch_size, time_steps, hidden_size) dot (batch_size, time_steps) => (batch_size, hidden_size)
-        context_vector = Dot(axes=[1, 1], name='context_vector')([hidden_states, attention_weights])
+        context_vector = Dot(axes=[1, 1], name='context_vector')([flattened, attention_weights])
         pre_activation = Concatenate(name='attention_output')([context_vector, h_t])
-        attention_vector = Dense(self.units, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
-        return attention_vector
-
-    def get_config(self):
-        return {'units': self.units}
+        attention_vector = Dense(np.prod(hidden_size), use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
+        _output = Reshape(hidden_size)(attention_vector)
+        return _output
 
     @classmethod
     def from_config(cls, config):
         return cls(**config)
+
+
+# from tensorflow.keras.layers import Dense, Lambda, Dot, Activation, Concatenate
+# from tensorflow.keras.layers import Layer
+
+
+# class Attention(Layer):
+
+#     def __init__(self, units=128, **kwargs):
+#         self.units = units
+#         super().__init__(**kwargs)
+
+#     def __call__(self, inputs):
+#         """
+#         Many-to-one attention mechanism for Keras.
+#         @param inputs: 3D tensor with shape (batch_size, time_steps, input_dim).
+#         @return: 2D tensor with shape (batch_size, 128)
+#         @author: felixhao28, philipperemy.
+#         """
+#         hidden_states = inputs
+#         hidden_size = int(hidden_states.shape[2])
+#         # Inside dense layer
+#         #              hidden_states            dot               W            =>           score_first_part
+#         # (batch_size, time_steps, hidden_size) dot (hidden_size, hidden_size) => (batch_size, time_steps, hidden_size)
+#         # W is the trainable weight matrix of attention Luong's multiplicative style score
+#         score_first_part = Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
+#         #            score_first_part           dot        last_hidden_state     => attention_weights
+#         # (batch_size, time_steps, hidden_size) dot   (batch_size, hidden_size)  => (batch_size, time_steps)
+#         h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
+#         score = Dot(axes=[1, 2], name='attention_score')([h_t, score_first_part])
+#         attention_weights = Activation('softmax', name='attention_weight')(score)
+#         # (batch_size, time_steps, hidden_size) dot (batch_size, time_steps) => (batch_size, hidden_size)
+#         context_vector = Dot(axes=[1, 1], name='context_vector')([hidden_states, attention_weights])
+#         pre_activation = Concatenate(name='attention_output')([context_vector, h_t])
+#         attention_vector = Dense(self.units, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
+#         return attention_vector
+
+#     def get_config(self):
+#         return {'units': self.units}
+
+#     @classmethod
+#     def from_config(cls, config):
+#         return cls(**config)
